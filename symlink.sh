7cde37760789c24dc87ae5938a65e1ee7eeec7a7
Rikard Wadman <rikard.wadman@volvo.com>
2022-05-21 18:31:56 +0200

    fix

--- symlink.sh
+++ symlink.sh
@@ -4,9 +4,10 @@
 mkdir -p "${HOME}/.vim/"
 
 cp -r "${HOME}/repos/conf/vim/colors" "${HOME}/.vim/"
+cp -r "${HOME}/repos/conf/vim/autoload" "${HOME}/.vim/"
 ln -sf "${HOME}/repos/conf/.vimrc" "${HOME}/.vimrc"
 
 
-# VSCode:
-ln -sf "${HOME}/repos/conf/vscode/settings.json" "${HOME}/.config/Code/User/settings.json"
-ln -sf "${HOME}/repos/conf/vscode/keybindings.json" "${HOME}/.config/Code/User/keybindings.json"
+# VSCode: (not same path on mac)
+# ln -sf "${HOME}/repos/conf/vscode/settings.json" "${HOME}/.config/Code/User/settings.json"
+# ln -sf "${HOME}/repos/conf/vscode/keybindings.json" "${HOME}/.config/Code/User/keybindings.json"
