334853fab4f36e1ef4563cbbd5ce5890cc1512af
Rikard Wadman <rikard.wadman@volvo.com>
2026-04-09 13:47:08 +0200

    i3 config

--- symlink.sh
+++ symlink.sh
@@ -11,3 +11,12 @@ ln -sf "${HOME}/repos/conf/.vimrc" "${HOME}/.vimrc"
 # VSCode: (not same path on mac)
 ln -sf "${HOME}/repos/conf/vscode/settings.json" "${HOME}/.config/Code/User/settings.json"
 ln -sf "${HOME}/repos/conf/vscode/keybindings.json" "${HOME}/.config/Code/User/keybindings.json"
+
+
+# i3:
+mkdir -p "${HOME}/.config/i3"
+ln -sf "${HOME}/repos/conf/i3/config" "${HOME}/.config/i3/config"
+
+# Symlink helper scripts to ~/.i3/
+mkdir -p "${HOME}/.i3"
+ln -sf "${HOME}/repos/conf/i3/polybar.sh" "${HOME}/.i3/polybar.sh"
